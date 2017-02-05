f6eab33ff2cdd75f604d3db9d389875d85170f5f
Pruxis <llavaert@gmail.com>
2017-02-06 00:52:42 +0100

    Replacing navbar with header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="page-header">
+        <div className="row">
+          <h1 className="text-center">Make your playlists here</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
