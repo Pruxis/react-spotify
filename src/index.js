1edc9a8c5c28b823fb3bfff5a6ff51f79c1cbef4
Pruxis <llavaert@gmail.com>
2017-02-06 00:18:39 +0100

    Rewriting index.js to es6 syntax

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import App from './components/App';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={App} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root />, document.querySelector('#root'));
