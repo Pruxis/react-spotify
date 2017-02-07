5303da348287e5bb8e6bff20d4c2ac5a9ffb930a
Pruxis <llavaert@gmail.com>
2017-02-07 19:13:03 +0100

    Adding track component

---
+++ src/components/Track.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Track extends Component {
+    render() {
+        return (
+            <div className="help-block">
+                <div className="panel panel-default">
+                    <div className="panel-body">
+                        <h4>Title</h4>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default Track;
